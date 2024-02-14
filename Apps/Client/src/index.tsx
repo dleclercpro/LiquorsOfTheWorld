ebb15728018e15b9a00cdb02a0c633a80d6143e7
David Leclerc <d.leclerc.pro@gmail.com>
2024-02-14 23:14:17 +0100

    Redux boilerplate.

--- Apps/Client/src/index.tsx
+++ Apps/Client/src/index.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { ENV } from './utils/env';
 import { setLogLevel } from './utils/logging';
-import { AppContextProvider } from './contexts/AppContextProvider';
+import App from './App';
+import './index.scss';
 
 setLogLevel(ENV);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
