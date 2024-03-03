c15df8be3608a217ad69162226668fae6a12cddf
David Leclerc <d.leclerc.pro@gmail.com>
2024-03-03 09:57:10 +0100

    Feat: restart Docker Compose file and pull images if necessary with Bash script.

--- Scripts/run.local.sh
+++ Scripts/run.local.sh
@@ -1,17 +1,14 @@
+# Define constant image details
+user="dleclercpro"
+app="liquors-quiz"
+release="latest"
+composefile="docker-compose.local.yml"
+
 # Get the directory containing the script
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Move to the root directory
-cd "${dir}/.."
-
-# Move to server app directory
-cd "./Apps/Server"
-
-# Start Redis instance
-docker compose up -d
-
-# Move back to root directory
-cd "${dir}/.."
+# Fetch newer images
+docker compose pull
 
 # Start app instance
-docker compose -f docker-compose.local.yml up
+docker compose -f $composefile up --force-recreate
