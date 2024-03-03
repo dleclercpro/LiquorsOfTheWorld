1e622676828cf92f5ca02d80fbb326e54b383339
David Leclerc <d.leclerc.pro@gmail.com>
2024-03-03 05:56:11 +0100

    Fix: speak to root to address server.

--- Apps/Client/src/config/index.ts
+++ Apps/Client/src/config/index.ts
@@ -2,7 +2,7 @@ import { Environment } from '../constants';
 
 export const ENV = process.env.NODE_ENV;
 export const DEBUG = ENV === Environment.Development;
-export const SERVER_ROOT = DEBUG ? `http://localhost:8000` : `http://liquors-quiz-app:8000`;
+export const SERVER_ROOT = DEBUG ? `http://localhost:8000` : ``;
 export const API_ROOT = `${SERVER_ROOT}/api/v1`;
 export const REFRESH_STATUS_INTERVAL = 5_000; // (ms)
 
