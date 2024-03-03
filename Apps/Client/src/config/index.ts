37a129266b09e21770f08024a931aac492597852
David Leclerc <d.leclerc.pro@gmail.com>
2024-03-03 05:44:48 +0100

    Test.

--- Apps/Client/src/config/index.ts
+++ Apps/Client/src/config/index.ts
@@ -2,13 +2,13 @@ import { Environment } from '../constants';
 
 export const ENV = process.env.NODE_ENV;
 export const DEBUG = ENV === Environment.Development;
-export const SERVER_ROOT = DEBUG ? `http://localhost:8000` : ``;
+export const SERVER_ROOT = DEBUG ? `http://localhost:8000` : `http://liquors-quiz-app:8000`;
 export const API_ROOT = `${SERVER_ROOT}/api/v1`;
 export const REFRESH_STATUS_INTERVAL = 5_000; // (ms)
 
 export const BACKGROUND_URLS = [
-  `${SERVER_ROOT}/static/img/bg/liquors/background-1.jpg`,
-  `${SERVER_ROOT}/static/img/bg/liquors/background-2.jpg`,
-  `${SERVER_ROOT}/static/img/bg/liquors/background-3.jpg`,
-  `${SERVER_ROOT}/static/img/bg/liquors/background-4.webp`,
-];
+  `background-1.jpg`,
+  `background-2.jpg`,
+  `background-3.jpg`,
+  `background-4.webp`,
+].map(filename => `${SERVER_ROOT}/static/img/bg/liquors/${filename}`);
