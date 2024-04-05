c84f1c9fee623481cc8d5b85cd4c1e3dbb69d2c4
David Leclerc <d.leclerc.pro@gmail.com>
2024-04-05 19:31:10 +0200

    Release tag.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,7 +4,7 @@ dir="$(cd "$(dirname "$0")" && pwd)"
 # Define image details
 user="dleclercpro"
 app="quiz"
-release="latest"
+release="v3.0.0"
 
 # Build multi-platform Docker image and push it to Dockerhub
 docker buildx build --platform linux/amd64,linux/arm64 -t $user/$app:$release -f ./Dockerfile . --push
