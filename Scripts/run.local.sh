4629ad1af0971990a6e416f126e57d5fe6af35f1
David Leclerc <d.leclerc.pro@gmail.com>
2024-06-10 12:25:17 +0200

    Release tag.

--- Scripts/run.local.sh
+++ Scripts/run.local.sh
@@ -1,7 +1,7 @@
 # Define constant image details
 user="dleclercpro"
 app="quiz"
-release="latest"
+release="v3.9.0"
 composefile="docker-compose.local.yml"
 
 # Get and move to the directory containing the script
