a03662641a84920b39a508347d8465c06e7a9064
David Leclerc <d.leclerc.pro@gmail.com>
2024-06-18 05:21:54 +0200

    Fix: missing parts in script.

--- Scripts/release.sh
+++ Scripts/release.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 # This is a shebang line that tells the system to use the bash shell to interpret this script
 
-# Get and move to the directory containing the script
-DIR="$(cd "$(dirname "$0")" && pwd)"
-ROOT_DIR="$DIR/../"
-APPS_DIR="$ROOT_DIR/Apps"
-SCRIPTS_DIR="$ROOT_DIR/Scripts"
 
-# Define branch details
-user="dleclercpro"
-app="quiz"
-master="latest"
-master_branch="$user/$app:$master"
+
+# Function to validate the release version format
+validate_release_format() {
+  if [[ ! "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Error: Release version must be in the format X.Y.Z where X, Y, and Z are integers."
+    exit 1
+  fi
+}
+
+
+
+# Directories
+SCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
+ROOT_DIR="$SCRIPTS_DIR/../"
+APPS_DIR="$ROOT_DIR/Apps"
+
+# Files
+INSTALL_SCRIPT="$SCRIPTS_DIR/install.sh"
+NPM_FILES=("$APPS_DIR/Server/package.json" "$APPS_DIR/Client/package.json")
+DOCKER_FILES=("$ROOT_DIR/docker-compose.yml" "$ROOT_DIR/docker-compose.local.yml")
+SCRIPT_FILES=("$SCRIPTS_DIR/build.sh" "$SCRIPTS_DIR/run.sh" "$SCRIPTS_DIR/run.local.sh")
+
+# Branch details
+USER="dleclercpro"
+APP="quiz"
+MASTER_TAG="latest"
+MASTER_BRANCH="$USER/$APP:$MASTER_TAG"
+MASTER_BRANCH_NAME="master"
+
+
 
 # Check if the release version is provided as an argument
 if [ -z "$1" ]; then
@@ -19,16 +39,24 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-# Set the release variable to the provided argument
-release="$1"
-release_tag="v$release"
-release_branch="$user/$app:$release"
-release_branch_name="release/$release"
+# Validate the release version format
+validate_release_format "$1"
+
+# Ensure GitHub CLI is installed
+if ! command -v gh &> /dev/null
+then
+    echo "GitHub CLI (gh) could not be found. Please install it and try again."
+    exit 1
+fi
+
+
+
+# Set CWD to scripts directory
+cd "$ROOT_DIR"
 
 
 
 # Check out the new branch with Git
-cd "$ROOT_DIR"
 if git show-ref --verify --quiet "refs/heads/$release_branch_name"; then
   git checkout "$release_branch_name"
 else
@@ -37,16 +65,29 @@ fi
 
 
 
-# Define the list of files to update
-PACKAGE_JSON_FILES=("$APPS_DIR/Server/package.json" "$APPS_DIR/Client/package.json")
-SCRIPT_FILES=("$DIR/build.sh" "$DIR/run.sh" "$DIR/run.local.sh")
-YAML_FILES=("$ROOT_DIR/docker-compose.yml" "$ROOT_DIR/docker-compose.local.yml")
+# Set the release variable to the provided argument
+release="$1"
+release_tag="v$release"
+release_branch="$USER/$APP:$release"
+release_branch_name="release/$release"
+
+
 
 # Replace "version": "latest" with "version": "$release" in .json files
-for file in "${PACKAGE_JSON_FILES[@]}"; do
+for file in "${NPM_FILES[@]}"; do
   if [ -f "$file" ]; then
-    sed -i "s|\"version\": \"$master\"|\"version\": \"$release\"|g" "$file"
+    sed -i "s|\"version\": \"$MASTER_TAG\"|\"version\": \"$release\"|g" "$file"
     echo "Updated version in '$file'."
+  else
+    echo "File '$file' not found!"
+  fi
+done
+
+# Replace the old branch with the new branch in Docker files
+for file in "${DOCKER_FILES[@]}"; do
+  if [ -f "$file" ]; then
+    sed -i "s|$MASTER_BRANCH|$release_branch|g" "$file"
+    echo "Updated branch in '$file'."
   else
     echo "File '$file' not found!"
   fi
@@ -55,18 +96,8 @@ done
 # Replace release="latest" with release="$release" in .sh files
 for file in "${SCRIPT_FILES[@]}"; do
   if [ -f "$file" ]; then
-    sed -i "s|release=\"$master\"|release=\"$release\"|g" "$file"
+    sed -i "s|release=\"$MASTER_TAG\"|release=\"$release\"|g" "$file"
     echo "Updated release in '$file'."
-  else
-    echo "File '$file' not found!"
-  fi
-done
-
-# Replace the old branch with the new branch in YAML files
-for file in "${YAML_FILES[@]}"; do
-  if [ -f "$file" ]; then
-    sed -i "s|$master_branch|$release_branch|g" "$file"
-    echo "Updated branch in '$file'."
   else
     echo "File '$file' not found!"
   fi
@@ -75,8 +106,6 @@ done
 
 
 # Run the install script before pushing the changes
-INSTALL_SCRIPT="$SCRIPTS_DIR/install.sh"
-
 if [ -f "$INSTALL_SCRIPT" ]; then
   bash "$INSTALL_SCRIPT"
 else
@@ -94,3 +123,9 @@ git commit -m "Release tag: '$release_tag'."
 
 # Push the new branch to the repository
 git push -u origin "$release_branch_name"
+
+# Create a new release on GitHub and tag it using release_tag
+gh release create "$release_tag" --title "$release_tag" --notes "Automated release for version: $release_tag"
+
+# Check out the master branch
+git checkout "$MASTER_BRANCH_NAME"
